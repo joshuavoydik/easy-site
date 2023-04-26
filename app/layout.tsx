282e9b5aae95e3ba8664578d2b9ed23682742728
Joshua <voydik@gmail.com>
2023-04-26 15:46:26 -0700

    dsfds

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,15 +14,15 @@ const kaisei = localFont({
 
 export const metadata: Metadata = {
   title: {
-    default: 'Lee Robinson',
-    template: '%s | Lee Robinson',
+    default: 'Joshua Voydik',
+    template: '%s | Joshua Voydik',
   },
   description: 'Developer, writer, and creator.',
   openGraph: {
     title: 'Lee Robinson',
     description: 'Developer, writer, and creator.',
-    url: 'https://leerob.io',
-    siteName: 'Lee Robinson',
+    url: 'https://voydik.com',
+    siteName: 'Joshua Voydik',
     images: [
       {
         url: 'https://leerob.io/og.jpg',
